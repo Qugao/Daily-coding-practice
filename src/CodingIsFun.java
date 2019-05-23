a18b6bc28463992a4e741b402a9bad70d47df618
Quan Gao <gaoquan196@gmail.com>
2019-05-22 21:35:59 -0700

    May 22 Update
    lc #215, #1, #344, #347, #21

--- src/CodingIsFun.java
+++ src/CodingIsFun.java
@@ -1,10 +1,17 @@
 import java.lang.reflect.Array;
 import java.util.*;
 
+
 public class CodingIsFun {
-    public static int[] largestAndSecond(int[] array) {
-        if (array.length <= 1) {
-            return array;
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    //lc #215
+    public static int findKthLargest(int[] nums, int k) {
+        if (nums.length <= 1) {
+            return nums[0];
         }
         PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
             @Override
@@ -15,17 +22,21 @@ public class CodingIsFun {
                 return o1 > o2 ? -1 : 1;
             }
         });
-        int[] result = {0, 0};
 
-        for (int i = 0; i < array.length; i++) {
-            pq.add(array[i]);
+        //int[] result = new int[k];
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(nums[i]);
         }
 
-        result[0] = pq.poll();
-        result[1] = pq.poll();
+        for (int j = 0; j < k; j++) {
+            result = pq.poll();
+        }
         return result;
     }
 
+
+    // lc #1 : Two Sum
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         int[] result = {0, 0};
@@ -43,7 +54,7 @@ public class CodingIsFun {
 
         return result;
     }
-
+    // lc #344
     public static void reverseString(char[] s) {
         int j = s.length - 1;
         for (int i = 0; i < s.length / 2; i++, j--) {
@@ -59,6 +70,71 @@ public class CodingIsFun {
         return true;
     }
 
+    // lc #347
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int num : nums) {
+            Integer freq = freqMap.get(num);
+
+            if (freq == null) {
+                freqMap.put(num, 1);
+            } else {
+                freqMap.put(num, freq + 1);
+            }
+        }
+
+        //System.out.println(freqMap);
+
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(k, new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o1.getValue().compareTo(o2.getValue());
+            }
+        });
+
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            if (minHeap.size() < k) {
+                minHeap.offer(entry);
+            } else if (entry.getValue() > minHeap.peek().getValue()) {
+                minHeap.poll();
+                minHeap.offer(entry);
+            }
+        }
+        //System.out.println(minHeap);
+
+        for (int i = minHeap.size() - 1; i >= 0; i--) {
+            result.add(minHeap.poll().getKey());
+        }
+
+        return result;
+    }
+
+    // lc #21
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+
+        while (l1 != null && l2 != null) {
+             if (l1.val > l2.val) {
+                 cur.next = l2;
+                 l2 = l2.next;
+             } else {
+                 cur.next = l1;
+                 l1 = l1.next;
+             }
+             cur = cur.next;
+        }
+
+        if (l1 != null) {
+            cur.next = l1;
+        } else {
+            cur.next = l2;
+        }
+        return dummy.next;
+    }
+
     public static void main(String[] args) {
         char[] a = {'h','e','l','l','o'};
         reverseString(a);
@@ -66,5 +142,8 @@ public class CodingIsFun {
 
         String b = "{[]}";
         System.out.println(isValid(b));
+
+        int[] c = {1,1,1,2,2,3};
+        System.out.println(topKFrequent(c, 2));
     }
 }
